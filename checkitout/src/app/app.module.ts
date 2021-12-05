bc8e92da042c308a63a2d1aa5dd522d1b6d8dee8
NoahKirsch20 <54324590+NoahKirsch20@users.noreply.github.com>
2021-12-04 22:01:15 -0600

    Made admin add-item page into a grid like system, everything is not so big

--- checkitout/src/app/app.module.ts
+++ checkitout/src/app/app.module.ts
@@ -66,7 +66,8 @@ import { AdminGuard } from './services/admin.guard';
     ScrollingModule,
     RouterModule.forRoot([
       { path: 'reset', component: ResetComponent },
-      { path: 'admin', component: AdminFacingComponent, canActivate: [AdminGuard], pathMatch: 'full' },
+      { path: 'admin', component: AdminFacingComponent, //canActivate: [AdminGuard], 
+      pathMatch: 'full' },
       { path: 'cart', component: CartComponent },
       { path: 'checkout/:model', component: ItemComponent },
       { path: 'about', component: AboutComponent },
