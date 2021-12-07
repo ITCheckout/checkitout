e1c092e77c006d0704bc3793028d3c06cc8bf50e
Shawn Nguyen <shawnnguyen@Shawns-MacBook-Pro.local>
2021-12-06 23:34:15 -0600

    Minor responsive changes for Home and Checkout pages. Added 'Popular Items' code to Admin page. Refracted code for Admin UI (Forms were resized, applied Flex Wrap, centered forms).

--- checkitout/src/app/admin-facing/admin-facing.component.ts
+++ checkitout/src/app/admin-facing/admin-facing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { DatabaseService } from '../services/database.service';
 import { MatTabsModule } from '@angular/material/tabs';
 
 interface Category {
@@ -16,7 +17,17 @@ export class AdminFacingComponent implements OnInit {
 
 
 
-  constructor(private afAuth: AngularFireAuth) { }
+  constructor(private afAuth: AngularFireAuth, private databaseService: DatabaseService) { }
+
+  mbp2015 = [];
+  mbp2015Available = 0;
+  mbp2015Unavailable = 0;
+  mbp2017 = [];
+  mbp2017Available = 0;
+  mbp2017Unavailable = 0;
+  dell = [];
+  dellAvailable = 0;
+  dellUnavailable = 0;
 
   ngOnInit(): void {
     //log if user is logged in
@@ -29,6 +40,10 @@ export class AdminFacingComponent implements OnInit {
     });
 
     console.log(this.afAuth.currentUser);
+
+    this.mbp2015 = this.databaseService.getItemCount('Mid 2015 MacBook Pro');
+    this.mbp2017 = this.databaseService.getItemCount('Mid 2017 MacBook Pro');
+    this.dell = this.databaseService.getItemCount('Dell Latitude 5580');
   }
 
   selectedValue: string | undefined;
